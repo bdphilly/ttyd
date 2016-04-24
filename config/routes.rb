9ac221049fd509615b99198c886be9272435870f
bryan phillips <bdphilly@gmail.com>
2016-04-24 14:38:45 -0700

    try adding type to javascript_include_tag

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,6 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
-  get ':path', to: 'home#index', format: false
   get '*path' => redirect('/')
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
